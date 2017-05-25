7b118ecfa98a6af196e82ed31a5176137437d8d5
PengZiqiao <xfyx.student@sina.com>
2017-05-25 11:50:36 +0800

    make it better

--- app/views.py
+++ app/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def index(request):
     title = "首页"
-    name = "peng"
+    header = "Django模板<small>测试版</small>"
     return render(request, "base.html", locals())
